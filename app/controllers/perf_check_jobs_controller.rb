7fd9da299ecee99d431c7e9e9ca90ec150f199c3
Nathan Bertram <nbertram@gmail.com>
2018-05-07 23:22:29 -0700

    Use load_perf_check_jobs

--- app/controllers/perf_check_jobs_controller.rb
+++ app/controllers/perf_check_jobs_controller.rb
@@ -1,12 +1,8 @@
 class PerfCheckJobsController < ApplicationController
   before_action :find_perf_check_job, only: [:show]
+  before_action :load_perf_check_jobs, except: [:create]
 
   def index
-    if params[:search].present?
-      @perf_check_jobs = PgSearch.multisearch(params[:search]).page(params[:page]).per(params[:per]).map(&:searchable)
-    else
-      @perf_check_jobs = PerfCheckJob.page(params[:page]).per(params[:per])
-    end
   end
 
   def new
@@ -28,6 +24,14 @@ class PerfCheckJobsController < ApplicationController
   end
 
   private
+
+  def load_perf_check_jobs
+    if params[:search].present?
+      @perf_check_jobs = PgSearch.multisearch(params[:search]).page(params[:page]).per(params[:per]).map(&:searchable)
+    else
+      @perf_check_jobs = PerfCheckJob.page(params[:page]).per(params[:per])
+    end  
+  end
 
   def perf_check_job_params
     params.require(:perf_check_job).permit!
