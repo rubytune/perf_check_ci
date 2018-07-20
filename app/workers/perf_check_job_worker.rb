ce6cac038c3fb1a8d0d7e843dc74f3ef38853dd6
Nathan Bertram <nbertram@gmail.com>
2018-07-20 14:12:15 -0700

    Have the job run for 45 seconds for now to replicate running state

--- app/workers/perf_check_job_worker.rb
+++ app/workers/perf_check_job_worker.rb
@@ -5,9 +5,9 @@ class PerfCheckJobWorker
     perf_check_job = PerfCheckJob.find(perf_check_job_id)
 
     if perf_check_job.run_benchmarks!
-      perf_check_job.completed!
+      perf_check_job.complete!
     else
-      perf_check_job.failed!
+      perf_check_job.fail!
     end
   end
 end
