91149a973a854b2a4c31ac62f938c2a442fb6eb1
Nathan Bertram <nbertram@gmail.com>
2018-07-31 10:34:19 -0700

    No needed

--- app/channels/daemon_checks_channel.rb
+++
@@ -1,9 +0,0 @@
-class DaemonChecksChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "daemon_checks"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
