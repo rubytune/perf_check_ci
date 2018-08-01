fe9cd2523cfbe70f8c81db8f63c7ffe3e9efefc7
Nathan Bertram <nbertram@gmail.com>
2018-07-31 23:09:39 -0700

    Move logger logic into it's own concern

---
+++ app/models/concerns/perf_check_job_log.rb
@@ -0,0 +1,42 @@
+module PerfCheckJobLog
+  extend ActiveSupport::Concern
+
+  included do
+    def log_root_dir
+      Rails.root
+    end
+
+    def log_file_dir
+      "/log/perf_check_jobs/"
+    end
+
+    def create_empty_log_file!
+      log_filename =  "perf-check-job-#{id}.log"
+      FileUtils.touch("#{log_root_dir}#{log_file_dir}/#{log_filename}")
+      update_attribute(:log_filename, log_filename)
+    end
+
+    def log_path
+      log_file_dir + log_filename
+    end
+
+    def full_log_path
+      "#{log_root_dir}#{log_path}"
+    end
+
+    def read_log_file
+      return "*** ERROR: Log File Not Found ***\nDirectory: #{log_path}" unless File.exists?(full_log_path)
+      contents = File.read(full_log_path, encoding: "UTF-8")
+      return "*** Empty Log File ***\nDirectory: #{log_path}" if contents.blank?
+      contents
+    end
+
+    def broadcast_log_file!(log_contents = nil)
+      ActionCable.server.broadcast("log_notifications_channel", {id: id, contents: log_contents || read_log_file, status: status})
+    end
+
+    def should_broadcast_log_file?
+      !(completed? || failed? || canceled?)
+    end
+  end
+end
--- app/models/perf_check_job.rb
+++ app/models/perf_check_job.rb
@@ -1,5 +1,6 @@
 class PerfCheckJob < ActiveRecord::Base
   include PerfCheckJobStatemachine
+  include PerfCheckJobLog
   include PgSearch
   multisearchable :against => [:username, :branch, :status]
 
@@ -49,46 +50,5 @@ class PerfCheckJob < ActiveRecord::Base
 
   def create_clone_and_rerun!
     PerfCheckJob.create(clone_params)
-  end
-
-  ############
-  # Log File
-  ############
-
-  def log_root_dir
-    Rails.root
-  end
-
-  def log_file_dir
-    "/log/perf_check_jobs/"
-  end
-
-  def create_empty_log_file!
-    log_filename =  "perf-check-job-#{id}.log"
-    FileUtils.touch("#{log_root_dir}#{log_file_dir}/#{log_filename}")
-    update_attribute(:log_filename, log_filename)
-  end
-
-  def log_path
-    log_file_dir + log_filename
-  end
-
-  def full_log_path
-    "#{log_root_dir}#{log_path}"
-  end
-
-  def read_log_file
-    return "*** ERROR: Log File Not Found ***\nDirectory: #{log_path}" unless File.exists?(full_log_path)
-    contents = File.read(full_log_path, encoding: "UTF-8")
-    return "*** Empty Log File ***\nDirectory: #{log_path}" if contents.blank?
-    contents
-  end
-
-  def broadcast_log_file!(log_contents = nil)
-    ActionCable.server.broadcast("log_notifications_channel", {id: id, contents: log_contents || read_log_file, status: status})
-  end
-
-  def should_broadcast_log_file?
-    !(completed? || failed? || canceled?)
   end
 end
