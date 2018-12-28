c585caac20608f9799098063bcb5e65c3b3c8bc4
Nathan Bertram <nbertram@gmail.com>
2018-12-28 05:00:50 -0800

    More args

--- app/javascript/controllers/perf_check_wizard_controller.js
+++ app/javascript/controllers/perf_check_wizard_controller.js
@@ -1,28 +1,40 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "numberOfRuns", "finalCommand" ]
+  static targets = [ "numberOfRuns", "branchName", "finalCommand", "onlyBenchBranch", "runMigrations", "verifyNoDiff" ]
 
   connect() {
     this.numberOfRunsTarget.value = '20'
-
+    this.branchNameTarget.value = 'master'
     this.setFinalCommand()
   }
-
-
-  // greet() {
-  //   this.numberOfRunsTarget.textContent = `Hello, ${this.numberOfRunsTarget.value}!`
-  // }
-
 
   // copy() {
   //   this.sourceTarget.select()
   //   document.execCommand("copy")
   // }
 
-  setFinalCommand() {
-    var numberOfRunsCmd = "-n " + this.numberOfRunsTarget.value;
+  generateArgumentString(target, prefix) {
+    if(target.value != undefined && target.value != ''){
+      return prefix + ' ' + target.value;
+    } else {
+      return '';
+    }
+  }
 
-    this.finalCommandTarget.value = numberOfRunsCmd;
+  generateArgumentBoolean(target, cmd) {
+    if(target.checked){
+      return cmd;
+    }
+  }
+
+  setFinalCommand() {
+    var numberOfRunsCmd = this.generateArgumentString(this.numberOfRunsTarget, '-n');
+    var branchNameCmd = this.generateArgumentString(this.branchNameTarget, '-r');
+    var onlyBenchBranchCmd = this.generateArgumentBoolean(this.onlyBenchBranchTarget, '-q')
+    var runMigrationsCmd = this.generateArgumentBoolean(this.runMigrationsTarget, '--run-migrations')
+    var verifyNoDiffCmd = this.generateArgumentBoolean(this.verifyNoDiffTarget, '--verify-no-diff')
+
+    this.finalCommandTarget.value = [numberOfRunsCmd, branchNameCmd, onlyBenchBranchCmd, runMigrationsCmd, verifyNoDiffCmd].join(' ');
   }
 }
