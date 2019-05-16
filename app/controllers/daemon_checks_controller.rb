4b5faff47e8d4ed92f189e8ea57510fe89570e0c
Sudara <sudara@alonetone.com>
2019-05-16 19:41:15 +0200

    Temporarily get production sidekiq_status working

--- app/controllers/daemon_checks_controller.rb
+++ app/controllers/daemon_checks_controller.rb
@@ -1,6 +1,6 @@
 class DaemonChecksController < ApplicationController
   def sidekiq_status
-    sidekiq_status = `ps aux | grep sidekiq`.include?("of 25 busy") ? 'online' : 'offline'
+    sidekiq_status = `ps aux | grep sidekiq`.include?("of 1 busy") ? 'online' : 'offline'
     render json: { status: sidekiq_status }
   end
 end
