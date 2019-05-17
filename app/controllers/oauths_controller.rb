ff58353b67131b2bb970e6cca02b51e5340ff680
Sudara <sudara@alonetone.com>
2019-05-17 18:03:51 +0200

    Sketch out asking github to verify membership before logging in

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,31 +1,33 @@
 class OauthsController < ApplicationController
   skip_before_action :require_user
   before_action :require_no_user
-      
+
   # sends the user on a trip to the provider,
   # and after authorizing there back to the callback url.
   def oauth
     session[:return_to_url] = request.referer unless request.referer =~ /oauth/
     login_at(params[:provider])
   end
-      
+
   def callback
-    provider = params[:provider]
-    # raise @user_hash.inspect
-    if @user = login_from(provider)
+    @params_from_github = params[:provider]
+
+    if @user = login_from(@params_from_github)
+      redirect_back_or_to root_path
+    elsif github_organization_permitted?
+      @user = create_from(@params_from_github)
+      auto_login(@user)
       redirect_back_or_to root_path
     else
-      # begin
-
-        @user = create_from(provider)
-
-        redirect_back_or_to root_path
-
-        reset_session # protect from session fixation attack
-        auto_login(@user)
-      # rescue
-      #   redirect_back_or_to root_path
-      # end
+      flash[:error] = "Sorry, your github account is not in the whitelist of github organizations allowed"
+      redirect_back_or_to root_path
     end
+  end
+
+  protected
+
+  def github_organization_permitted?
+    @github = Octokit::Client.new(access_token: @access_token)
+    @github.organization_member?(APP_CONFIG[:github_organization], @params_from_github[:github_username])
   end
 end
