40e31326a3c862248ed34605eeb537cebe8651af
Manfred Stienstra <manfred@fngtps.com>
2019-07-22 16:08:44 +0200

    Leave only authorization in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,7 @@
 class ApplicationController < ActionController::Base
-  include Pagy::Backend
-  protect_from_forgery with: :exception
-  before_action :require_user, except: [:health]
-  
-  def health
-    head :ok
-  end
+  include Authentication
 
-  private
-
-  def require_user
-    if current_user.blank?
-      redirect_to login_path
-      return false
-    end
-  end
-
-  def require_no_user
-    if current_user.present?
-      redirect_to root_path
-      return false
-    end
-  end
-
-  def load_perf_check_jobs
-    if params[:search].present?
-      @perf_check_jobs = PgSearch.multisearch(params[:search]).page(params[:page]).per(params[:per]).map(&:searchable)
-    else
-      @perf_check_jobs, @perf_check_jobs_records = pagy(PerfCheckJob.most_recent)
-    end
-  end
+  before_action -> {
+    redirect_to new_session_url unless authenticated?
+  }
 end
