0a62462f546a320f749c51e2bc3de7fda903701d
Manfred Stienstra <manfred@fngtps.com>
2019-07-22 15:35:52 +0200

    Update test sessions controller to use auth. concern.

--- test/integration/test/sessions_controller_test.rb
+++ test/integration/test/sessions_controller_test.rb
@@ -5,6 +5,6 @@ require_relative '../../test_helper'
 class Test::SessionsControllerTest < ActionDispatch::IntegrationTest
   test "sets session user ID so they're authenticated" do
     post '/test/sessions', params: { user_id: 42 }
-    assert_equal '42', session[:user_id]
+    assert_equal 42, session[:user_id]
   end
 end
