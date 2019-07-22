64b5080841bef9e24668832122614f8d7f02ee46
Manfred Stienstra <manfred@fngtps.com>
2019-07-22 15:38:40 +0200

    Remove Sorcery from the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ApplicationRecord
-  authenticates_with_sorcery! do |config|
-    config.authentications_class = Authentication
-  end
-
-  validates :first_name, :last_name, :email, :github_username, presence: true
-
   has_many :perf_check_jobs, dependent: :destroy
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
@@ -12,5 +6,4 @@ class User < ApplicationRecord
   def full_name=(name)
     self.first_name, self.last_name = name.split(" ")
   end
-
 end
