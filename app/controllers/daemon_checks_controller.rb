c6726a24c60703578c846c63732a5e67fbf46e8c
Manfred Stienstra <manfred@fngtps.com>
2019-07-25 15:15:43 +0200

    Don't skip Sorcery callback; it's not there.

--- app/controllers/daemon_checks_controller.rb
+++ app/controllers/daemon_checks_controller.rb
@@ -1,7 +1,4 @@
 class DaemonChecksController < ApplicationController
-  # Sorcery does this on every action, it's too aggressive for polling
-  skip_after_action :register_last_activity_time_to_db
-
   def sidekiq_status
     sidekiq_status = `ps aux | grep sidekiq`.include?('of 1 busy') ? 'online' : 'offline'
     render json: { status: sidekiq_status }
