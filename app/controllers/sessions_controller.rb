185e8f002c5bf728edf91a41578a16ab0e23055b
Manfred Stienstra <manfred@fngtps.com>
2019-07-25 15:15:21 +0200

    Implement sessions controller with Authorization.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,42 @@
-class SessionsController < ApplicationController
-  skip_before_action :require_user, only: :new
-  before_action :require_no_user, only: [:new]
+# frozen_string_literal: true
 
+# Manages authentication sessions for users by authorizing
+# access to GitHub and verifying access to the configured
+# GitHub organization.
+class SessionsController < ActionController::Base
+  include Authentication
+
+  layout 'application'
+
+  # Step 1: Redirect to GitHub to start flow to get OAuth access token.
   def new
-    render layout: 'login'
+    redirect_to Authorization.new(authorization_params).url
+  end
+
+  # Step 2: User returns from GitHub with either OAuth code or an error message
+  # when something went wrong.
+  def show
+    @authorization = Authorization.new(authorization_params)
+    if user_id = @authorization.process
+      login(user_id)
+      redirect_to root_url
+    else
+      render :error
+    end
   end
 
   def destroy
     logout
-    redirect_to root_path
+    redirect_to root_url
   end
 
+  private
+
+  def authorization_params
+    params.permit(
+      :code, :state, :error, :error_description, :error_uri
+    ).merge(
+      callback_url: session_url
+    )
+  end
 end
---
+++ test/integration/sessions_controller_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  include Support::Github
+
+  setup do
+    @code = SecureRandom.alphanumeric
+    @state = SecureRandom.alphanumeric
+    @access_token = SecureRandom.alphanumeric
+    @name = 'Iorek Byrnison'
+    @login = 'iorek'
+    @organizations = APP_CONFIG['github_organizations'] || []
+  end
+
+  test 'starts a new authentication session' do
+    get '/session/new'
+    assert(
+      response.location.start_with?(
+        Authorization.github_authorize_url
+      )
+    )
+  end
+
+  test 'creates a new session when authentication is successful' do
+    mock_oauth
+
+    get '/session', params: { code: @code, state: @state }
+    assert_redirected_to root_url
+    assert_not_nil session[:user_id]
+  end
+
+  test 'sees error when authentication failed' do
+  end
+
+  test 'destroys an authenticated session' do
+    login :lyra
+
+    delete '/session'
+    assert_redirected_to root_url
+    assert_nil session[:user_id]
+  end
+end
