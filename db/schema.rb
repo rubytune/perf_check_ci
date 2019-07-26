fbe4cfe934e6f72e1effdc5ad4c3e75d2d722cfc
Manfred Stienstra <manfred@fngtps.com>
2019-07-26 16:01:03 +0200

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_134942) do
+ActiveRecord::Schema.define(version: 2019_07_26_135950) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "perf_check_job_test_cases", force: :cascade do |t|
