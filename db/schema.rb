5db5b2a736f4bfc3d0d5570eb2834a5da820e51e
Manfred Stienstra <manfred@fngtps.com>
2019-07-26 15:50:59 +0200

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_115824) do
+ActiveRecord::Schema.define(version: 2019_07_26_134942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,15 +50,6 @@ ActiveRecord::Schema.define(version: 2019_07_22_115824) do
     t.integer "user_id"
     t.string "github_html_url"
     t.index ["user_id"], name: "index_perf_check_jobs_on_user_id"
-  end
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "users", force: :cascade do |t|
