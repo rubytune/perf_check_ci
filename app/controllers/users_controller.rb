482d0a9c46a3515c29b54c642d72c1f23871ac08
Manfred Stienstra <manfred@fngtps.com>
2019-07-26 15:19:30 +0200

    Order jobs on user page so you can actually paginate.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   include Pagy::Backend
 
   def show
-    @user_perf_check_jobs, @user_perf_check_jobs_records = pagy(current_user.perf_check_jobs)
+    @user_perf_check_jobs, @user_perf_check_jobs_records = pagy(current_user.perf_check_jobs.order(updated_at: :desc))
     @disable_search_nav = true
   end
 end
