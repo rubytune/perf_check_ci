e596d272da96f01a9f421fe4f35689cd6b97e421
Manfred Stienstra <manfred@fngtps.com>
2019-07-26 17:15:42 +0200

    Rename PerfCheckJob to Job in workers.

--- app/workers/perf_check_job_worker.rb
+++ app/workers/perf_check_job_worker.rb
@@ -2,15 +2,15 @@ class PerfCheckJobWorker
   include Sidekiq::Worker
   sidekiq_options queue: :perf_check
 
-  def perform(perf_check_job_id)
+  def perform(job_id)
     sleep 0.25 # Give the DB a sec
-    perf_check_job = PerfCheckJob.find(perf_check_job_id)
-    LogNotificationsWorker.perform_async(perf_check_job_id)
+    job = Job.find(job_id)
+    LogNotificationsWorker.perform_async(job_id)
 
-    if perf_check_job.run_benchmarks!
-      perf_check_job.complete!
+    if job.run_benchmarks!
+      job.complete!
     else
-      perf_check_job.fail!
+      job.fail!
     end
   end
 end
