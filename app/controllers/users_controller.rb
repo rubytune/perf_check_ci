ab771c6d31b18c0ac6ca2d12efe5bbaae21a1824
Manfred Stienstra <manfred@fngtps.com>
2019-07-26 10:34:57 +0200

    Include pagination in users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include Pagy::Backend
+
   def show
     @user_perf_check_jobs, @user_perf_check_jobs_records = pagy(current_user.perf_check_jobs)
     @disable_search_nav = true
