4775bf25eb1455796e01d4c80a9b77f792869d1e
Manfred Stienstra <manfred@fngtps.com>
2019-08-07 16:02:43 +0200

    Don't queue the LogNotificationsWorker.

--- app/workers/perf_check_job_worker.rb
+++ app/workers/perf_check_job_worker.rb
@@ -5,8 +5,6 @@ class PerfCheckJobWorker
   def perform(job_id)
     sleep 0.25 # Give the DB a sec
     job = Job.find(job_id)
-    LogNotificationsWorker.perform_async(job_id)
-
     if job.run_benchmarks!
       job.complete!
     else
