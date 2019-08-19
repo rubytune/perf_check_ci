f464bdf7f3c3e34403d15b08f2abeca68a2410e9
Manfred Stienstra <manfred@fngtps.com>
2019-08-19 17:52:10 +0200

    Add a helper method to render ANSI colors to HTML.

--- app/helpers/job_helper.rb
+++ app/helpers/job_helper.rb
@@ -12,4 +12,37 @@ module JobHelper
   def user_type_options_for_select
     options_for_select(user_type_options)
   end
+
+  # Turns raw log output into nice HTML.
+  def render_log(log)
+    # Borrow private method from colorize gem.
+    log.send(:scan_for_colors).map do |mode, color, background, text|
+      if [mode, color, background].all?(&:nil?)
+        text
+      else
+        style = []
+
+        case mode.to_i
+        when 1
+          style << 'font-weight: bold;'
+        when 4
+          style << 'text-decoration: underline;'
+        end
+
+        if name = render_color(color.to_i - 30)
+          style << "color: #{name};"
+        end
+        if name = render_color(background.to_i - 40)
+          style << "background-color: #{name};"
+        end
+
+        content_tag(:span, text, style: style.join(' '))
+      end
+    end.join.gsub("\n", '<br>')
+  end
+
+  def render_color(number)
+    name = String.color_codes.key(number).to_s
+    name == 'default' ? nil : name
+  end
 end
--- test/helpers/job_helper_test.rb
+++ test/helpers/job_helper_test.rb
@@ -15,4 +15,24 @@ class JobHelperTest < ActionView::TestCase
       assert option.start_with?('<option')
     end
   end
+
+  # rubocop:disable Style/StringLiterals
+  test 'replaces ANSI colors in job output with spans with colors' do
+    result = render_log(
+      '[2019-08-10 16:56:21] 	[4;39;49mrequest[0m   ' \
+      '[4;39;49mlatency[0m   [4;39;49mserver rss[0m   ' \
+      '[4;39;49mstatus[0m   [4;39;49mqueries[0m   ' \
+      '[4;39;49mprofiler data[0m'
+    )
+    assert_equal(
+      "[2019-08-10 16:56:21] \t<span style=\"text-decoration: underline;\">" \
+      "request</span>   <span style=\"text-decoration: underline;\">latency" \
+      "</span>   <span style=\"text-decoration: underline;\">server rss" \
+      "</span>   <span style=\"text-decoration: underline;\">status</span>" \
+      "   <span style=\"text-decoration: underline;\">queries</span>   " \
+      "<span style=\"text-decoration: underline;\">profiler data</span>",
+      result
+    )
+  end
+  # rubocop:enable Style/StringLiterals
 end
