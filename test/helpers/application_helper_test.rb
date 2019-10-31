51cf9c6c7a13fa4f24804f5cffab346e50461b88
Manfred Stienstra <manfred@fngtps.com>
2019-10-31 15:22:11 +0100

    Remove unused helpers.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,12 +3,19 @@
 require_relative '../test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  test 'does not return current user when nobody is authenticated' do
-    assert_nil current_user
+  test 'formats time ago' do
+    assert time_ago(2.minutes.ago).include?('minutes')
+    assert time_ago(5.days.ago).include?('at')
   end
 
-  test 'returns the current user when someone is authenticated' do
-    @current_user = users(:lyra)
-    assert_equal users(:lyra), current_user
+  test 'formats validation errors' do
+    job = Job.new
+    assert_equal '', error_messages(job.errors)
+
+    job.errors.add(:experimental_branch, :blank)
+    assert_equal(
+      '<ul class="error"><li>Experimental branch can&#39;t be blank</li></ul>',
+      error_messages(job.errors)
+    )
   end
 end
