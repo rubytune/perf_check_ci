54669c0cb76c98aac73bbe32338604b8e0b46003
Manfred Stienstra <manfred@fngtps.com>
2019-11-05 18:49:59 +0100

    Add helper to format error messages.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,6 +3,9 @@
 require_relative '../test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
+  # Returns the currently authenticated user and nil otherwise.
+  attr_reader :current_user
+
   test 'does not return current user when nobody is authenticated' do
     assert_nil current_user
   end
@@ -10,5 +13,21 @@ class ApplicationHelperTest < ActionView::TestCase
   test 'returns the current user when someone is authenticated' do
     @current_user = users(:lyra)
     assert_equal users(:lyra), current_user
+  end
+
+  test 'formats time ago' do
+    assert time_ago(2.minutes.ago).include?('minutes')
+    assert time_ago(5.days.ago).include?('at')
+  end
+
+  test 'formats validation errors' do
+    job = Job.new
+    assert_equal '', error_messages(job.errors)
+
+    job.errors.add(:experiment_branch, :blank)
+    assert_equal(
+      '<ul class="error"><li>Experiment branch can&#39;t be blank</li></ul>',
+      error_messages(job.errors)
+    )
   end
 end
