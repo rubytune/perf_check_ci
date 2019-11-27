d54bf1c24e94bde80574a34589171d57e02dc639
Manfred Stienstra <manfred@fngtps.com>
2019-11-27 16:36:03 +0100

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_171031) do
+ActiveRecord::Schema.define(version: 2019_11_27_153137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_171031) do
     t.boolean "diff_response", default: true
     t.string "request_user_role"
     t.text "request_user_email"
+    t.json "measurements"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
